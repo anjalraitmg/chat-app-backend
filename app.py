3eada2809edd7c2b45799ea71c6d26e290d24846
anjalrai <anjalrai.tmg@gmail.com>
2025-09-25 22:27:59 +0545

    1.3

--- app.py
+++ app.py
@@ -1,31 +1,55 @@
-# backend/app.py - MODIFIED FOR PRODUCTION
-import os
-from flask import Flask
+from flask import Flask, request
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 
+# Create the Flask app
 app = Flask(__name__)
+# Add CORS to allow our React app to connect
+CORS(app, resources={r"/*": {"origins": "*"}}) 
 
-# Get the frontend URL from an environment variable for security
-# Use a default for local development
-frontend_url = os.environ.get('FRONTEND_URL', 'http://localhost:3000')
+# Create the Socket.IO server, wrapping the Flask app
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Make CORS more secure: only allow the frontend to connect
-CORS(app, resources={r"/*": {"origins": frontend_url}})
-socketio = SocketIO(app, cors_allowed_origins=frontend_url)
+# A dictionary to store connected users {session_id: username}
+connected_users = {}
 
 @socketio.on('connect')
 def handle_connect():
-    print('Client connected!')
+    """This function is called when a user opens the webpage."""
+    print(f'Client connected! SID: {request.sid}')
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    print('Client disconnected!')
+    """This function is called when a user closes the webpage."""
+    print(f'Client disconnected! SID: {request.sid}')
+    # Remove the user from our dictionary if they exist
+    if request.sid in connected_users:
+        username = connected_users.pop(request.sid)
+        print(f'User {username} has left.')
+        # Broadcast the new user list to all remaining clients
+        emit('user_list', list(connected_users.values()), broadcast=True)
+
+@socketio.on('join')
+def handle_join(username):
+    """This function is called when a user logs in with their name."""
+    # Store the user's name with their unique session ID
+    connected_users[request.sid] = username
+    print(f'User {username} has joined. SID: {request.sid}')
+    print(f'Current users: {list(connected_users.values())}')
+    # Broadcast the new, updated list of users to ALL clients
+    emit('user_list', list(connected_users.values()), broadcast=True)
 
 @socketio.on('message')
 def handle_message(data):
+    """This function is called when a server receives an event named 'message'."""
+    # data will be an object like {'author': 'user', 'message': 'text'}
     print(f'Received message from {data["author"]}: {data["message"]}')
+    
+    # The emit function will broadcast the entire data object to ALL connected clients.
     emit('message', data, broadcast=True)
 
-# We no longer need the __main__ block for production
-# Gunicorn will run the 'app' object directly
+# This is the entry point to run our server
+if __name__ == '__main__':
+    print("Starting server...")
+    # For production, we'll use a different command, but this is fine for local dev
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
